f5f1fac198a5d673b87283a50e1e8d2c55492403
Vachet Virginie <vachetvirginie@gmail.com>
2018-07-25 10:59:30 +0200

    ajout timer/refactorisation

---
+++ src/Timer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const TIMER = 5000;
+
+class Timer extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            timer: TIMER
+        }
+    }
+
+    render(){
+
+    }
+
+    start(){
+
+    }
+
+    stop(){
+
+    }
+
+    reset(){
+
+    }
+}
+
+export default Timer;
