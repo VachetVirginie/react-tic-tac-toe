7ea4ed01a202012a7ac00357fe1646391328c26f
Vachet Virginie <vachetvirginie@gmail.com>
2018-07-25 15:14:40 +0200

    ajout timer

--- src/Timer.js
+++ src/Timer.js
@@ -1,30 +1,57 @@
 import React from 'react';
 
 const TIMER = 5000;
+const DEC = 100;
 
-class Timer extends Component{
+class Timer extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            timer: TIMER
+             timer : TIMER
+            }
+    }
+        
+
+   
+    render() {
+        return (
+            
+            <div>
+            <h1>Timer:{this.state.timer / 1000}</h1>
+            <button onClick={() => this.start()}>Start</button>
+            <button onClick={() => this.stop()}>Stop</button>
+            <button onClick={() => this.reset()}>Reset</button>
+          </div>
+        );
         }
+        
+        
+
+    start() {
+        this.timer = setInterval(() =>{         
+            
+            if(this.state.timer <= 0){
+                this.stop();
+                return;
+            }
+            this.setState({
+                timer: this.state.timer - DEC
+            })
+
+        }, DEC)
     }
 
-    render(){
-
-    }
-
-    start(){
-
-    }
-
+   
+        
     stop(){
-
+        clearInterval(this.timer)
+        console.log("stop")
     }
 
     reset(){
-
+        this.stop();
+        this.setState({timer: TIMER})
     }
 }
 
